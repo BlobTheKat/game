a5774aafa93a5dcd529cce865ae53ab75fd73c43
blob <blob.kat@hotmail.com>
2021-10-11 15:39:36 +0100

    a

--- i.sh
+++
@@ -1,4 +0,0 @@
-wget -O server.js https://raw.githubusercontent.com/BlobTheKat/game/master/server.js
-wget -O ships https://raw.githubusercontent.com/BlobTheKat/game/master/ships
-wget -O asteroids https://raw.githubusercontent.com/BlobTheKat/game/master/asteroids
-node server.js $PORT
